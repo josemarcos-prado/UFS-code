a854be5379d5952da2654ede0d7a4b598310bea2
José Marcos <marcos.prado@dcomp.ufs.br>
2025-06-05 18:31:02 +0000

    inicio da minha versao do josephus

--- ED/ativ_pontuada/josephus.c
+++ ED/ativ_pontuada/josephus.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct no {
+    char nome[128];
+    struct no* proxNo;
+} tipoNo;
+
+typedef struct {
+    int quant;
+    tipoNo *inicio;
+    tipoNo *fim;
+} tipoLista;
+
 int main (void) {
+    
 
     return 0;
 }
